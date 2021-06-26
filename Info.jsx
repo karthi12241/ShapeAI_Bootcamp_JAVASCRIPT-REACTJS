6cb302b0c69c4e0735d766c143a1af7b7d19866f
karthi12241 <84267511+karthi12241@users.noreply.github.com>
2021-06-26 23:10:18 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>JAVASCRIPT AND REACTJS</h1>
+      <p>The Foundation of Web Development</p>
+    </div>
+  );
+}
+export default Info;
